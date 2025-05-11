8d8dfe35ec1eef9ee0c995b5ff146b7d61f8f120
karan-palan <karanpalan007@gmail.com>
2025-05-11 14:32:08 +0530

    chore: add clerk auth and fix bugs

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -18,12 +18,16 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 const endpoint = clusterApiUrl("devnet");
 const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
 
+import { ClerkProvider } from "@clerk/clerk-react";
+
 createRoot(document.getElementById("root")!).render(
-  <ConnectionProvider endpoint={endpoint}>
-    <WalletProvider wallets={wallets} autoConnect>
-      <WalletModalProvider>
-        <App />
-      </WalletModalProvider>
-    </WalletProvider>
-  </ConnectionProvider>
+  <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  </ClerkProvider>
 );
